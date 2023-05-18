c80ebded56bf6b4d0d744d3027e69b761404d8b2
Asraf Ansari <asraf3101aa@gmail.com>
2023-05-18 10:21:54 +0545

    Fetch using datatable

---
+++ ConsumeContactWebApi/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using ConsumeContactWebApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using Newtonsoft.Json;
+using System.Diagnostics;
+using System.Net.Http.Headers;
+using System.Text.Json.Serialization;
+
+namespace ConsumeContactWebApi.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        string baseURL = "https://localhost:7044/api/";
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            //Calling the web API and populating the data in view using DataTable
+            DataTable dataTable = new DataTable();
+            using(var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(baseURL);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage getData = await client.GetAsync("Contacts");
+                if (getData.IsSuccessStatusCode)
+                {
+                    string results = getData.Content.ReadAsStringAsync().Result;
+                    dataTable = JsonConvert.DeserializeObject<DataTable>(results);
+                }
+                else
+                {
+                    Console.WriteLine("Error calling with API");
+
+                }
+                ViewData.Model = dataTable;
+            }
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ConsumeContactWebApi/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ConsumeContactWebApi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
