ead9d9dbe9f2537d7a0528dc931457701f22e90b
Asraf Ansari <asraf3101aa@gmail.com>
2023-05-18 17:41:45 +0545

    Fetch and Add Contacts

--- ConsumeContactWebApi/Controllers/HomeController.cs
+++ ConsumeContactWebApi/Controllers/HomeController.cs
@@ -11,7 +11,7 @@ namespace ConsumeContactWebApi.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-        string baseURL = "https://localhost:7044/api/";
+        string baseURL = "https://localhost:7044/";
 
         public HomeController(ILogger<HomeController> logger)
         {
@@ -24,7 +24,7 @@ namespace ConsumeContactWebApi.Controllers
             DataTable dataTable = new DataTable();
             using(var client = new HttpClient())
             {
-                client.BaseAddress = new Uri(baseURL);
+                client.BaseAddress = new Uri(baseURL + "api/");
                 client.DefaultRequestHeaders.Accept.Clear();
                 client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
                 HttpResponseMessage getData = await client.GetAsync("Contacts");
@@ -39,6 +39,67 @@ namespace ConsumeContactWebApi.Controllers
 
                 }
                 ViewData.Model = dataTable;
+            }
+            return View();
+        }
+        public async Task<IActionResult> Index2()
+        {
+            //Calling the web API and populating the data in view using Entity Model Class
+            //DataTable dataTable = new DataTable();
+            IList<UserEntity> users = new List<UserEntity>();
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(baseURL + "api/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage getData = await client.GetAsync("Contacts");
+                if (getData.IsSuccessStatusCode)
+                {
+                    string results = getData.Content.ReadAsStringAsync().Result;
+                    //dataTable = JsonConvert.DeserializeObject<DataTable>(results);
+                    users = JsonConvert.DeserializeObject<List<UserEntity>>(results);
+                }
+                else
+                {
+                    Console.WriteLine("Error calling with API");
+
+                }
+                //ViewData.Model = dataTable;
+                ViewData.Model = users;
+            }
+            return View();
+        }
+
+        public async Task<ActionResult<String>> Contacts(UserEntity user)
+        {
+            UserEntity entity = new UserEntity()
+            {  
+                FullName = user.FullName,
+                Email = user.Email,
+                Phone = user.Phone,
+                Address = user.Address,
+
+            };
+            if (user.FullName != null && user.Phone != 0 && user.Email != null && user.Address != null)
+            {
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri(baseURL + "api/");
+                    client.DefaultRequestHeaders.Accept.Clear();
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                    HttpResponseMessage getData = await client.PostAsJsonAsync<UserEntity>("Contacts", entity);
+                    if (getData.IsSuccessStatusCode)
+                    {
+                        return RedirectToAction("Index","Home");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error calling with API");
+
+                    }
+                    
+                }
+
             }
             return View();
         }
