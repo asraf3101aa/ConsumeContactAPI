3ec68864ea62ede2b5bf45cd58128051da641123
Asraf Ansari <asraf3101aa@gmail.com>
2023-05-22 15:27:14 +0545

    Made changes for LOGIN with API

--- ConsumeContactWebApi/Controllers/HomeController.cs
+++ ConsumeContactWebApi/Controllers/HomeController.cs
@@ -5,13 +5,19 @@ using Newtonsoft.Json;
 using System.Diagnostics;
 using System.Net.Http.Headers;
 using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Newtonsoft.Json.Linq;
+using System.Text.Json.Nodes;
 
 namespace ConsumeContactWebApi.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-        string baseURL = "https://localhost:7044/";
+        string baseURL = "https://localhost:7022/";
 
         public HomeController(ILogger<HomeController> logger)
         {
@@ -104,9 +110,100 @@ namespace ConsumeContactWebApi.Controllers
             return View();
         }
 
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(baseURL + "api/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage getData = await client.GetAsync("Contacts/" + id);
+                if (getData.IsSuccessStatusCode)
+                {
+                    string results = getData.Content.ReadAsStringAsync().Result;
+                    JObject jsonObject = JObject.Parse(results);
+                    var viewModel = new UpdateContact()
+                    {
+                        Id = (Guid)jsonObject["id"],
+                        FullName = jsonObject["fullName"].ToString(),
+                        Email = jsonObject["email"].ToString(),
+                        Phone = (long)jsonObject["phone"],
+                        Address = jsonObject["address"].ToString()
+                    };
+                    return await Task.Run(() => View("View", viewModel));
+
+                }
+
+                return RedirectToAction("Index", "Home");
+
+            }
+        }
+
+        public async Task<IActionResult> View(UpdateContact updateContact)
+        {
+            UpdateContact entity = new UpdateContact()
+            {
+                FullName = updateContact.FullName,
+                Email = updateContact.Email,
+                Phone = updateContact.Phone,
+                Address = updateContact.Address,
+
+            };
+            if (updateContact.FullName != null && updateContact.Phone != 0 && updateContact.Email != null && updateContact.Address != null)
+            {
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri(baseURL + "api/");
+                    client.DefaultRequestHeaders.Accept.Clear();
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                    HttpResponseMessage getData = await client.PutAsJsonAsync<UpdateContact>("Contacts/"+ updateContact.Id, entity);
+                    if (getData.IsSuccessStatusCode)
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error calling with API");
+
+                    }
+
+                }
+
+            }
+
+
+            return RedirectToAction("Index", "Home");
+        }
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(baseURL + "api/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage getData = await client.DeleteAsync("Contacts/" + id);
+                if (getData.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    Console.WriteLine("Error calling with API");
+
+                }
+
+            }
+            return RedirectToAction("Index", "Home");
+        }
+
         public IActionResult Privacy()
         {
             return View();
+        }
+        public async Task<IActionResult> LogOut()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Login", "Access");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ ConsumeContactWebApi/Models/UpdateContact.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsumeContactWebApi.Models
+{
+    public class UpdateContact
+    {
+        public Guid Id { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public long Phone { get; set; }
+        public string Address { get; set; }
+    }
+}
